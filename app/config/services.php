d59e4898a2ff88cbfb54b56a70ae3436dd97e5a3
n0nag0n <n0nag0n@users.noreply.github.com>
2024-06-18 23:06:23 -0600

    Update services.php for db connection examples

--- app/config/services.php
+++ app/config/services.php
@@ -7,8 +7,13 @@ use flight\database\PdoWrapper;
  * @var array $config This comes from the returned array at the bottom of the config.php file
  * @var Engine $app
  */
-$dsn = 'mysql:host=' . $config['database']['host'] . ';dbname=' . $config['database']['dbname'] . ';charset=utf8mb4';
-$app->register('db', PdoWrapper::class, [ $dsn, $config['database']['user'], $config['database']['password'] ]);
+
+// uncomment the following line for MySQL
+// $dsn = 'mysql:host=' . $config['database']['host'] . ';dbname=' . $config['database']['dbname'] . ';charset=utf8mb4';
+
+// uncomment the following line for SQLite
+// $dsn = sqlite:' . $config['database']['file_path'];
+$app->register('db', PdoWrapper::class, [ $dsn, $config['database']['user'] ?? null, $config['database']['password'] ?? null ]);
 
 // Got google oauth stuff? You could register that here
 // $app->register('google_oauth', Google_Client::class, [ $config['google_oauth'] ]);
